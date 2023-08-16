47ae8d222383be1abd2d94b52a64271948468494
Zal-atan <ethanzalta@yahoo.com>
2023-08-16 13:39:08 -0700

    betty compliance

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,60 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_left - counts height to the left from specific node
- *
- * @tree: pointer to root of binary tree
- *
- * Return: Returns size of tree
- */
-/*size_t binary_tree_height(const binary_tree_t *tree, unsigned int height)
-{
-
-	if (tree == NULL)
-		return (0);
-
-	if (!tree->right && !tree->left)
-		return (leftHeight);
-
-	if (tree->left)
-		height += binary_tree_height(tree->left);
-
-	if (tree->right)
-		height += binary_tree_height(tree->right);
-
-	return (height);
-} */
-
-/**
- * binary_tree_height_right - counts height to the right from specific node
- *
- * @tree: pointer to root of binary tree
- *
- * Return: Returns size of tree
- */
-/*size_t binary_tree_height(const binary_tree_t *tree, unsigned int height)
-{
-
-	if (tree == NULL)
-		return (0);
-
-	if (!tree->right && !tree->left)
-		return (height);
-
-	if (tree->parent != NULL)
-		height += 1;
-
-	if (tree->left)
-		height += binary_tree_height(tree->left);
-
-	if (tree->right)
-		height += binary_tree_height(tree->right);
-
-	return (height);
-} */
-
-
-/**
  * binary_tree_height - counts height from specific node
  *
  * @tree: pointer to root of binary tree
