19c198114e0367b0d1ba6ce19fb43cc15cfca548
Zal-atan <ethanzalta@yahoo.com>
2023-08-16 11:39:05 -0700

    Trying Insert Left but with binary_tree_node function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,23 +10,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode;
+	binary_tree_t *oldLeft, *newNode;
 
 	if (parent == NULL)
 		return (NULL);
-	newNode = malloc(sizeof(binary_tree_t));
-	if (!newNode)
-		return (NULL);
 
-	newNode->parent = parent;
-	if (parent->left != NULL)
-	{
-		newNode->left = parent->left;
-		newNode->left->parent = newNode;
-	}
+	oldLeft = parent->left;
+
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+		return (NULL);
 	parent->left = newNode;
-	newNode->right = NULL;
-	newNode->n = value;
+
+	newNode->left = oldLeft;
+	if (oldLeft != NULL)
+	{
+		oldLeft->parent = newNode;
+	}
 
 	return (newNode);
 
