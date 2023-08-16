906b9587c8826320318c08488fb9695668c2500c
Zal-atan <ethanzalta@yahoo.com>
2023-08-16 11:27:21 -0700

    Trying number 1 again

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newNode->parent = parent;
-	if (parent->left)
+	if (parent->left != NULL)
 	{
 		newNode->left = parent->left;
 		newNode->left->parent = newNode;
