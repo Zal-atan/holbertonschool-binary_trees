9a43e0bf944de29afcb9dcc49877014f8e39bc80
Zal-atan <ethanzalta@yahoo.com>
2023-08-17 12:32:44 -0700

    Second try of is balanced

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,58 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the total leaves of given binary tree
+ * binary_tree_height - counts height from specific node
  *
  * @tree: pointer to root of binary tree
  *
- * Return: Returns number of leaves
+ * Return: Returns size of tree
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	unsigned int totalLeaves = 0;
+	int leftHeight = 0, rightHeight = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		totalLeaves += binary_tree_leaves(tree->left);
-
-	if (tree->right)
-		totalLeaves += binary_tree_leaves(tree->right);
-
 	if (!tree->right && !tree->left)
-		return (totalLeaves + 1);
-
-	else
-		return (totalLeaves);
-}
-
-/**
- * binary_tree_nodes - counts the total nodes of given binary tree
- *
- * @tree: pointer to root of binary tree
- *
- * Return: Returns number of nodes
- */
-size_t binary_tree_nodes(const binary_tree_t *tree)
-{
-	unsigned int totalNodes = 0;
-
-	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		totalNodes += binary_tree_nodes(tree->left);
+	{
+		leftHeight += 1;
+		leftHeight += binary_tree_height(tree->left);
+	}
 
 	if (tree->right)
-		totalNodes += binary_tree_nodes(tree->right);
+	{
+		rightHeight += 1;
+		rightHeight += binary_tree_height(tree->right);
+	}
 
-	if (!tree->right && !tree->left)
-		return (totalNodes);
+	if (leftHeight > rightHeight)
+		return (leftHeight);
 
 	else
-		return (totalNodes + 1);
+		return (rightHeight);
 }
+
 
 /**
  * binary_tree_balance - measures the balance of tree
@@ -70,14 +53,14 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree->left)
 	{
-		leftWeight += binary_tree_nodes(tree->left);
-		leftWeight += binary_tree_leaves(tree->left);
+		leftWeight += 1;
+		leftWeight += binary_tree_height(tree->left);
 	}
 
 	if (tree->right)
 	{
-		rightWeight += binary_tree_nodes(tree->right);
-		rightWeight += binary_tree_leaves(tree->right);
+		rightWeight += 1;
+		rightWeight += binary_tree_height(tree->right);
 	}
 
 	balance = (leftWeight - rightWeight);
